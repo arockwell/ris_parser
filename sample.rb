9f77f8776e77ed137f75d3a661658c44ecba97e4
Alex Rockwell <alexhr@ufl.edu>
2010-07-22 11:54:39 -0400

    Handling end or record now

--- sample.rb
+++ sample.rb
@@ -5,7 +5,7 @@ require 'treetop'
 require 'polyglot'
 require 'ris'
 
-blarg = "AR  - blarg\nAR  - foo\n"
+blarg = "AR  - blarg\nAR  - foo\nER  - \n"
 parser = RisParser.new
 
 puts parser.parse(blarg).eval
