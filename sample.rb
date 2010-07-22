032c358089f3b510b8b9eadd8f6f9591c0fe7243
Alex Rockwell <alexhr@ufl.edu>
2010-07-22 12:19:52 -0400

    Collates multiple AR's into an array

--- sample.rb
+++ sample.rb
@@ -4,8 +4,26 @@ require 'rubygems'
 require 'treetop'
 require 'polyglot'
 require 'ris'
+require 'pp'
 
-blarg = "AR  - blarg\nAR  - foo\nER  - \n"
+blarg = "AR  - blarg\nAR  - foo\nER  - \nAR  - test\nAR  - quux\nER  - \n"
 parser = RisParser.new
 
-puts parser.parse(blarg).eval
+a = parser.parse(blarg).eval
+records = []
+a.each do |record|
+  entry = {}
+  record.each do |key_value|
+    if key_value[0] == 'AR'
+      if entry[key_value[0]] == nil
+        entry[key_value[0]] = [key_value[1]]
+      else
+        entry[key_value[0]].push(key_value[1])
+      end
+    end
+  end
+  records << entry
+end
+
+pp records
+
