684143a6d744378803d4db02f139874e3de67ba2
Alex Rockwell <alexhr@ufl.edu>
2010-07-22 23:26:17 -0400

    Added real world testing data and fixed bugs in handling multiple records.

--- sample.rb
+++ sample.rb
@@ -6,7 +6,36 @@ require 'polyglot'
 require 'ris'
 require 'pp'
 
-blarg = "AR  - blarg\nAR  - foo\nER  - \nAR  - test\nAR  - quux\nER  - \n"
+blarg = <<-EOH
+TY  - CHAP
+ID  - 3092
+A1  - Adelson,R. T.
+T1  - Wound healing
+Y1  - 2008
+N1  - p. cm.; Entry: 20080425; Update: 20081111
+A2  - Thomas,Regan
+T2  - Advanced Therapy in Facial/Plastic Reconstructive Surgery
+CY  - B C Decker Inc
+SN  - 9781550093759; 1550093754 Vendor Info: Baker and Taylor Baker & Taylor YBP Library Services (BTCP BKTY YANK) 179.00 Status: active
+M1  - Book, Section
+ER  - 
+
+TY  - CHAP
+ID  - 3095
+A1  - Adelson,R. T.
+T1  - Nasal fracture
+Y1  - 2007
+SP  - 163
+EP  - 163-167
+A2  - Roland,P. S.
+T2  - Otolaryngology Clinical Case Studies Oral Board Exam Review
+PB  - Anadem Publishing, Inc.
+CY  - Columbus, OH
+M1  - Book, Section
+ER  - 
+
+EOH
+
 parser = RisParser.new
 
 a = parser.parse(blarg).eval
@@ -14,12 +43,14 @@ records = []
 a.each do |record|
   entry = {}
   record.each do |key_value|
-    if key_value[0] == 'AR'
+    if key_value[0] == 'A1' || key_value[0] == 'A2' || key_value[0] == 'KW'
       if entry[key_value[0]] == nil
         entry[key_value[0]] = [key_value[1]]
       else
         entry[key_value[0]].push(key_value[1])
       end
+    else
+      entry[key_value[0]] = key_value[1]
     end
   end
   records << entry
